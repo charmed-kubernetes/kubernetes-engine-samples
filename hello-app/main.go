ff0c013e9b1c9551546a64c922de5032a5089192
Mateo Florido <mateo.florido@canonical.com>
2022-05-20 19:03:29 -0500

    Modify image to allow multiarch build

--- hello-app/main.go
+++ hello-app/main.go
@@ -44,10 +44,7 @@ func main() {
 // hello responds to the request with a plain-text "Hello, world" message.
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
-	host, _ := os.Hostname()
 	fmt.Fprintf(w, "Hello, world!\n")
-	fmt.Fprintf(w, "Version: 1.0.0\n")
-	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
 
 // [END container_hello_app]
